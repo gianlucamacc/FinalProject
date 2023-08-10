3067473c4d7127cc31751131a994aa0518a57256
Chris <evan0285@algonquinlive.com>
2023-08-10 17:56:40 -0400

    able to rollback commit (For javadocs)

--- app/src/main/java/algonquin/cst2335/finalproject/DetailsFragment.java
+++ app/src/main/java/algonquin/cst2335/finalproject/DetailsFragment.java
@@ -1,7 +1,3 @@
-/**
- * A Fragment class that displays detailed information about a selected TriviaScores object.
- * This class is responsible for inflating the layout and populating it with data.
- */
 package algonquin.cst2335.finalproject;
 
 import android.os.Bundle;
@@ -15,31 +11,14 @@ import algonquin.cst2335.finalproject.databinding.DetailsLayoutBinding;
 
 public class DetailsFragment extends Fragment {
 
-    private TriviaScores selected;
-
-    /**
-     * Constructs a new DetailsFragment with the provided TriviaScores object.
-     *
-     * @param t The TriviaScores object to display detailed information for.
-     */
+    TriviaScores selected;
     public DetailsFragment(TriviaScores t){
         selected = t;
     }
 
-    /**
-     * Called to create the content view for this Fragment.
-     *
-     * @param inflater           The LayoutInflater object that can be used to inflate any views in the fragment.
-     * @param container          The parent view that the fragment's UI should be attached to.
-     * @param savedInstanceState If non-null, this fragment is being re-constructed from a previous saved state.
-     * @return                   The View for the fragment's UI, or null.
-     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        // Inflate the layout using the generated binding class
         DetailsLayoutBinding binding = DetailsLayoutBinding.inflate(inflater);
-
-        // Set the timeTaken TextView with the selected TriviaScores object's timeTaken value
         binding.timeTaken.setText(selected.timeTaken);
 
         return binding.getRoot();
--- app/src/main/java/algonquin/cst2335/finalproject/Score_RecyclerViewAdapter.java
+++ app/src/main/java/algonquin/cst2335/finalproject/Score_RecyclerViewAdapter.java
@@ -1,50 +1,46 @@
-/**
- * A RecyclerView adapter for displaying trivia scores in a list format.
- * This adapter populates the RecyclerView with score data and provides a way to customize the view.
- */
 package algonquin.cst2335.finalproject;
 
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class Score_RecyclerViewAdapter extends RecyclerView.Adapter<Score_RecyclerViewAdapter.MyViewHolder> {
 
-    private static Context context;
-    private ArrayList<TriviaQuestionModel> questionModels;
-    private static List<TriviaScores> ByScoreList;
-    private TriviaDatabase db;
-    private static TriviaDAO tDAO;
+    static Context context;
+    ArrayList<TriviaQuestionModel> questionModels;
+    static List<TriviaScores> ByScoreList;
+    TriviaDatabase db;
+    static TriviaDAO tDAO;
 
-    /**
-     * Constructs a new Score_RecyclerViewAdapter.
-     *
-     * @param context       The context of the application.
-     * @param ByScoreList   A list of TriviaScores objects containing score information.
-     * @param db            The TriviaDatabase instance used for database operations.
-     */
+
     public Score_RecyclerViewAdapter(Context context, List<TriviaScores> ByScoreList, TriviaDatabase db) {
         this.context = context;
         this.ByScoreList = ByScoreList;
         this.db = db;
         this.tDAO = db.tDAO();
+
     }
 
-    /**
-     * Called when RecyclerView needs a new ViewHolder of the given type to represent an item.
-     *
-     * @param parent   The ViewGroup into which the new View will be added after it is bound to an adapter position.
-     * @param viewType The view type of the new View.
-     * @return         A new ViewHolder that holds a View of the given view type.
-     */
+
     @NonNull
     @Override
     public Score_RecyclerViewAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -53,47 +49,42 @@ public class Score_RecyclerViewAdapter extends RecyclerView.Adapter<Score_Recycl
         return new Score_RecyclerViewAdapter.MyViewHolder(view);
     }
 
-    /**
-     * Called to display the data at the specified position.
-     *
-     * @param holder   The ViewHolder that holds the View.
-     * @param position The position of the item within the adapter's data set.
-     */
     @Override
     public void onBindViewHolder(@NonNull Score_RecyclerViewAdapter.MyViewHolder holder, int position) {
+
         holder.userNameRecycler.setText(ByScoreList.get(position).userName);
         holder.scoreRecycler.setText(ByScoreList.get(position).scoreString);
         holder.scoreCategory.setText(ByScoreList.get(position).category);
+
+//        holder.answerView3.setText(questionModels.get(position).getAnswerList().get(3));
+//        holder.answerView4.setText(questionModels.get(position).getAnswerList().get(2));
+//        holder.tempAnswer.setText((questionModels.get(position).getCorrectAnswer()));
+//        holder.answerView4.setText(questionModels.get(position).getCorrectAnswer());
+//        holder.questionView.setText(questionModels.get(position).getQuestion());
+
     }
 
-    /**
-     * Returns the total number of items in the data set held by the adapter.
-     *
-     * @return The total number of items.
-     */
     @Override
     public int getItemCount() {
         return ByScoreList.size();
     }
 
-    /**
-     * ViewHolder class that represents an item view in the RecyclerView.
-     */
     public class MyViewHolder extends RecyclerView.ViewHolder {
         TextView userNameRecycler;
         TextView scoreRecycler;
         TextView scoreCategory;
 
-        /**
-         * Constructs a new MyViewHolder.
-         *
-         * @param itemView The View representing an item in the RecyclerView.
-         */
+
+
+
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             userNameRecycler = itemView.findViewById(R.id.userNameRecyclerScore);
             scoreRecycler = itemView.findViewById(R.id.scoreRecyclerScore);
             scoreCategory = itemView.findViewById(R.id.scoreCategory);
+
+
+
         }
     }
 }
